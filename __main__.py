815af1aad5cb3ac538d75754008bfaf614410a06
Adam Gordon Bell <v-adam@pulumi.com>
2025-04-16 11:31:05 -0400

    Refactor Pulumi program to use EKS and VPC, replacing S3 bucket creation and updating dependencies in requirements.txt.

--- __main__.py
+++ __main__.py
@@ -1,10 +1,24 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+import pulumi_awsx as awsx
+import pulumi_eks as eks
+import pulumi_kubernetes as k8s
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2('my-bucket')
+# Create VPC with default CIDR
+eks_vpc = awsx.ec2.Vpc("eks-vpc",
+    enable_dns_hostnames=True)
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# Create minimal EKS cluster
+eks_cluster = eks.Cluster("eks-cluster",
+    vpc_id=eks_vpc.vpc_id,
+    private_subnet_ids=eks_vpc.private_subnet_ids,
+    fargate=True,
+)
+
+# Create Kubernetes provider
+k8s_provider = k8s.Provider('k8s-provider',
+    kubeconfig=eks_cluster.kubeconfig
+)
+
+pulumi.export("kubeconfig", eks_cluster.kubeconfig)
+pulumi.export("vpc_id",eks_vpc.vpc_id)
+# pulumi.export("url", pulumi.Output.concat("http://", service.status.load_balancer.ingress[0].hostname))
