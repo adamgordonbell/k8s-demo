4881ec6d53b11b615cd880769c7ad0cb33040b73
Adam Gordon Bell <v-adam@pulumi.com>
2025-04-21 10:09:37 -0400

    Refactor: Move EKS cluster and related resources to eks-cluster directory; create new workload directory structure and files.

---
+++ eks-cluster/__main__.py
@@ -0,0 +1,22 @@
+import pulumi
+import pulumi_awsx as awsx
+import pulumi_eks as eks
+import pulumi_kubernetes as k8s
+
+# Create VPC with default CIDR
+eks_vpc = awsx.ec2.Vpc("eks-vpc",
+    enable_dns_hostnames=True)
+
+# Create minimal EKS cluster with EC2 nodes
+eks_cluster = eks.Cluster("eks-cluster",
+    vpc_id=eks_vpc.vpc_id,
+    private_subnet_ids=eks_vpc.private_subnet_ids,
+    skip_default_node_group=False,
+    instance_type="t3.micro",
+    desired_capacity=2,
+    min_size=1,
+    max_size=3,
+)
+
+pulumi.export("kubeconfig", eks_cluster.kubeconfig)
+pulumi.export("vpc_id",eks_vpc.vpc_id)
---
+++ workload/__main__.py
@@ -0,0 +1,5 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+from pulumi_aws import s3
+
