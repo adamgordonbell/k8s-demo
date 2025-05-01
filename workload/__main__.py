f71375a89f0ce9fe5dc37786ddb67ffaa4fe68f9
Adam Gordon Bell <v-adam@pulumi.com>
2025-05-01 15:39:56 -0400

    Add Kubernetes integration to Pulumi program and retrieve EKS kubeconfig.

--- workload/__main__.py
+++ workload/__main__.py
@@ -1,5 +1,12 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
 
+from pulumi_aws import s3
+import pulumi_kubernetes as k8s
+
+config = pulumi.Config()
+org = config.require("org")
+stack = pulumi.get_stack()
+
+# Reference the EKS cluster stack to get kubeconfig
+eks_stack = pulumi.StackReference(f"{org}/KubeKitties/{stack}")
+kubeconfig = eks_stack.get_output("kubeconfig")
